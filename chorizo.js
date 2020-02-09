e084d6bb3dd0509fccefd308265aa67e2cca2f26
alexisr <alexisr15494@gmail.com>
2020-02-09 16:15:45 -0300

    cambiando a es6

--- chorizo.js
+++ chorizo.js
@@ -7,8 +7,8 @@ function Chorizo (lote, kilo, color){
         return "este chorizo es del lote " + this.lote + ", pesa: " + this.kilo + ",de color: " + this.color;
     }
 }
-var ch1 = new Chorizo(1234, 5, "azul");
-var ch2 = new Chorizo(89832, 1, "rojo");
+let ch1 = new Chorizo(1234, 5, "azul");
+let ch2 = new Chorizo(89832, 1, "rojo");
 
 console.log(ch1.descripcion());
 console.log(ch2.descripcion());
