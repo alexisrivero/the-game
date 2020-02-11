aedb52c833d81b3df2c867f92f63be961317993a
alexisr <alexisr15494@gmail.com>
2020-02-11 20:38:58 -0300

    borre logo de import

--- the-game/src/App.js
+++ the-game/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Chorizo from './components/Chorizo';
 import Thegame from './components/Thegame';
