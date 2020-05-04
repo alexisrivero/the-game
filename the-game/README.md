50329a640046c2732fcc13a80f04c1aae274e7fb
alexisr <alexisr15494@gmail.com>
2020-05-04 15:01:38 -0300

    pre y post the game a la sugerencia

--- the-game/README.md
+++ the-game/README.md
@@ -1,4 +1,3 @@
 -- arreglar datosempresa, borrarlo
--- cambiar sugerencia para que busque/filtre el valor en cualquier posicion, no al principio
 
 
--- the-game/src/components/Sugerencia.js
+++ the-game/src/components/Sugerencia.js
@@ -5,21 +5,28 @@ const Sugerencia = (props) => {
     
 
     let thegame_1 = props.nombre.toLowerCase().replace(props.busqueda, props.busqueda);
+    //if(props.nombre.toLowerCase().indexOf(props.busqueda) != -1){
+        // la re concha de tu hermana
+    //}
+
     let thegame_3 = props.nombre.slice(props.busqueda.length);
     let thegame_4 = props.nombre.toLowerCase().split(props.busqueda)[1];
     let thegame_6 = props.nombre.substring(props.busqueda.length);
+
 
     let busqueda_display = props.busqueda.charAt(0).toUpperCase() + props.busqueda.slice(1);
 
     let elotro_game = props.nombre.toLowerCase().includes(props.busqueda)
     console.log(elotro_game)
 
-    let pre_thegame = "";
-    let post_thegame = thegame_1;
+    let final_thegame = props.nombre.toLowerCase().split(props.busqueda);
+    let pre_thegame = final_thegame[0];
+    let post_thegame = thegame_4;
+
 
     console.log(post_thegame)
 
-    return (<div className="Sugerencia">{pre_thegame}<span></span>{post_thegame}</div>)
+    return (<div className="Sugerencia">{pre_thegame}<span>{props.busqueda}</span>{post_thegame}</div>)
 }
 
 
